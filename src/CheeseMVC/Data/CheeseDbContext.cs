b21b60a8a89b2605d937dc2734688ffcfb995e71
McKinney <mckinney123@aol.com>
2017-12-19 15:00:12 -0600

    First leg

--- src/CheeseMVC/Data/CheeseDbContext.cs
+++ src/CheeseMVC/Data/CheeseDbContext.cs
@@ -1,15 +1,50 @@
 ﻿using CheeseMVC.Models;
 using Microsoft.EntityFrameworkCore;
 
+
+
 namespace CheeseMVC.Data
+
 {
+
     public class CheeseDbContext : DbContext
+
     {
+
         public DbSet<Cheese> Cheeses { get; set; }
 
-        public CheeseDbContext(DbContextOptions<CheeseDbContext> options) 
+        public CheeseDbContext(DbContextOptions<CheeseDbContext> options)
+
             : base(options)
+
         { }
 
+        public DbSet<CheeseCategory> Categories { get; set; }
+
     }
+
 }
+
+
+
+/*public class CheeseDbContext : DbContext
+{
+    public DbSet<Cheese> Cheeses { get; set; }
+    public DbSet<CheeseCategory> Categories { get; set; }
+    public DbSet<Menu> Menu { get; set; }
+    public DbSet<CheeseMenu> CheeseMenus { get; set; }
+
+
+    public CheeseDbContext(DbContextOptions<CheeseDbContext> options) 
+        : base(options)
+    { }
+
+    protected override void OnModelCreating(ModelBuilder modelbuilder)
+    {
+        modelBuilder.Entity<CheeseMenu>()
+            .HasKey(c => new{ c.CheeseID, c.MenuID });
+    } 
+
+    public DbSet<CheeseCategory> Categories { get; set; }
+}
+}*/
